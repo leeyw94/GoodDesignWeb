ec841eac0a8f3cb25cf5552705206c1a39cf3584
용우 이 <ceo@theblueeye.com>
2019-12-12 02:32:27 +0900

    이용우 home

--- SmartFactory/Controllers/CalendarController.cs
+++ SmartFactory/Controllers/CalendarController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using static SmartFactory.Controllers.SysController;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace SmartFactory.Controllers
 {
@@ -67,6 +68,13 @@ namespace SmartFactory.Controllers
                     o => o.index_order).Select( c => new { 값 = c.project_id, 이름 =  c.project_name  });
             ViewBag.프로젝트 = new SelectList(project_main.AsEnumerable(), "값", "이름");
             //======================================================================================================================================================== 
+
+            //========================================================================================================================================================
+            var _department = db.department.Where(p => p.company_idx == company_idx && p.use_yn != "N").OrderBy(P => P.index_order).Select(c => new { 값 = c.idx, 이름 = c.department_name });
+            ViewBag.부서 = new SelectList(_department.AsEnumerable(), "값", "이름");
+
+            //========================================================================================================================================================
+
 
 
             if (!string.IsNullOrEmpty(open))
@@ -230,121 +238,110 @@ namespace SmartFactory.Controllers
             return View(doc);
         }
 
-        //[Authorize]
-        //public ActionResult SaveCal(work_list doc)
-        //{
-        //    string mode = Request["mode"];
-        //    string _type = Request["_type"];
-        //    string web_company_id = Request.Cookies["web_company_id"].Value ?? "";
+        [Authorize]
+        public ActionResult calendar_action(calendar doc, string mode, string _type)
+        {
 
-        //    string msg = "";
+            #region 기본 사용자 정보
+            string user_id = User.Identity.Name;
+            int department_idx = Convert.ToInt32(UserData.user_get(user_id, "department_idx"));
+            int company_idx = Convert.ToInt32(UserData.user_get(user_id, "company_idx"));
+            int auth = Convert.ToInt32(UserData.user_get(user_id, "auth"));
+            #endregion
 
-        //    if (doc.open_it == "on")
-        //    {
-        //        doc.open_it = "Y";
-        //    }
-        //    else
-        //    {
-        //        doc.open_it = "N";
-        //    }
+          
+                       
 
-
-        //    DateTime sdate = doc.work_date;
-        //    DateTime edate = doc.end_date;
-
-        //    TimeSpan ts = edate - sdate;
-
-        //    double run_time = ts.Hours;
-
-        //    if (mode == "W")
-        //    {
-        //        #region 저장
-
-        //        doc.company_id = web_company_id;
-        //        doc.user_id = User.Identity.Name;
-        //        doc.write_date = DateTime.Now;
-        //        doc.use_yn = "Y";
-
-        //        db.work_list.Add(doc);
-        //        db.SaveChanges(); // 실제로 저장 
+            if (doc.open_it == "on")
+            {
+                doc.open_it = "Y";
+            }
+            else
+            {
+                doc.open_it = "N";
+            }
 
 
-        //        msg = Util.msg.msg_insert;
+            DateTime sdate = doc.start_date;
+            DateTime edate = doc.end_date;
 
-        //        #endregion
+            TimeSpan ts = edate - sdate;
 
-        //        if (_type == "B")
-        //        {
-        //            return Content("<script>alert('" + msg + "');  location.href='/Calendar/memo_list';</script>");
-        //        }
-        //        else
-        //        {
-        //            return Content("<script>alert('" + msg + "');  location.href='/Calendar/workcal';</script>");
-        //        }
-        //    }
-        //    else if (mode == "M")
-        //    {
-        //        //수정일경우
+            double run_time = ts.Hours;
 
-        //        #region 권한체크
+            if (mode == "W")
+            {
+                #region 저장
 
-        //        string user = User.Identity.Name;
+                doc.company_idx = company_idx;
+                doc.user_id = User.Identity.Name;
+                doc.write_date = DateTime.Now;
+                doc.use_yn = "Y";
 
-        //        int idx_it = Convert.ToInt32(Request["idx"]);
-        //        int auth = Convert.ToInt16(Request.Cookies["check_auth"].Value);
-        //        string web_department_id = Request.Cookies["web_department_id"].Value ?? "";
+                db.calendar.Add(doc);
+                db.SaveChanges(); // 실제로 저장 
 
+                           
 
-        //        string ok_auth = "N";
+                #endregion
 
-        //        if (doc.user_id == user)
-        //        {
-        //            ok_auth = "Y";
-        //        }
-        //        if (auth >= 8)
-        //        {
-        //            ok_auth = "Y";
-        //        }
-        //        if (web_department_id == doc.department_id)
-        //        {
-        //            ok_auth = "Y";
-        //        }
+               
+             
+               
+            }
+            else if (mode == "M")
+            {
+                //수정일경우
 
-        //        #endregion
+                #region 권한체크
 
-        //        if (ok_auth == "Y")
-        //        {
-        //            #region 기본 수정
+                string user = User.Identity.Name;
 
-        //            doc.user_id = User.Identity.Name;
-        //            doc.who = User.Identity.Name;
-        //            doc.company_id = web_company_id;
-        //            doc.write_date = DateTime.Now;
-        //            doc.edit_date = DateTime.Now;
-        //            db.Entry(doc).State = EntityState.Modified;
-        //            db.SaveChanges();
+            
 
-        //            msg = Util.msg.msg_edit;
+                string ok_auth = "N";
 
-        //            #endregion
+                if (doc.user_id == user)
+                {
+                    ok_auth = "Y";
+                }
+                if (auth >= 8)
+                {
+                    ok_auth = "Y";
+                }
+                if (department_idx == doc.department_idx)
+                {
+                    ok_auth = "Y";
+                }
 
-        //            if (_type == "B")
-        //            {
-        //                return Content("<script>alert('" + msg + "');  location.href='/Calendar/memo_list';</script>");
-        //            }
-        //            else
-        //            {
-        //                return Content("<script>alert('" + msg + "');  location.href='/Calendar/workcal';</script>");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            return Content("<script>alert('권한이 없습니다.');  location.href='/Calendar/workcal';</script>");
-        //        }
-        //    }
+                #endregion
 
-        //    return Content("<script>alert('잘못된 요청입니다.');  location.href='/Calendar/workcal';</script>");
-        //}
+                if (ok_auth == "Y")
+                {
+                    #region 기본 수정
+
+                    doc.user_id = User.Identity.Name;
+                    doc.who = User.Identity.Name;
+                    doc.company_idx = company_idx;
+                    doc.write_date = DateTime.Now;
+                    doc.edit_date = DateTime.Now;
+                    db.Entry(doc).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                
+
+                    #endregion
+
+                   
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+            }
+
+            return Redirect("/Calendar/calendar_view");
+        }
 
 
         //[Authorize]
@@ -450,115 +447,59 @@ namespace SmartFactory.Controllers
         //}
 
 
-        //[HttpPost]
-        //public string cal_update()
-        //{
-        //    DateTime start = Convert.ToDateTime(Request["start"].Replace("T", " "));
-        //    DateTime end = Convert.ToDateTime(Request["end"].Replace("T", " "));
+        [HttpPost]
+        public string cal_update(string start, string end, string w, string d, int idx)
+        {
+            DateTime _start = Convert.ToDateTime(start.Replace("T", " "));
+            DateTime _end = Convert.ToDateTime(end.Replace("T", " "));
 
-        //    string user = User.Identity.Name;
-        //    string state = "N";
-        //    string user_id = Request["w"];
-        //    string department_id = Request["d"];
+            string user = User.Identity.Name;
+            string state = "N";
+            string user_id = w;
+            string department_id = d;
 
-        //    int idx_it = Convert.ToInt32(Request["idx"]);
-        //    int auth = Convert.ToInt16(Request.Cookies["check_auth"].Value);
-        //    string web_department_id = Request.Cookies["web_department_id"].Value ?? "";
+            int idx_it = idx;
 
-        //    string ok_auth = "N";
-
-        //    if (user_id == user)
-        //    {
-        //        ok_auth = "Y";
-        //    }
-        //    if (auth >= 8)
-        //    {
-        //        ok_auth = "Y";
-        //    }
-        //    if (web_department_id == department_id)
-        //    {
-        //        ok_auth = "Y";
-        //    }
+            #region 기본 사용자 정보
+        
+            int department_idx = Convert.ToInt32(UserData.user_get(user_id, "department_idx"));
+            int company_idx = Convert.ToInt32(UserData.user_get(user_id, "company_idx"));
+            int auth = Convert.ToInt32(UserData.user_get(user_id, "auth"));
+            #endregion
 
 
-        //    if (ok_auth == "Y")
-        //    {
-        //        #region 칼린더 드래그 수정
+            string ok_auth = "N";
 
-        //        work_list _update =
-        //            (from a in db.work_list where a.idx == idx_it select a).FirstOrDefault();
-
-        //        _update.work_date = start;
-        //        _update.end_date = end;
-        //        _update.edit_date = DateTime.Now;
-
-        //        db.SaveChanges(); // 실제로 저장  
-        //        state = "Y";
-
-        //        #endregion
-        //    }
-        //    return state;
-        //}
-
-        //[HttpPost]
-        //public string cal_memo_insert()
-        //{
-        //    DateTime start = Convert.ToDateTime(Request["day"]);
-        //    DateTime end = start;
-
-        //    string user = User.Identity.Name;
-        //    string state = "N";
-        //    string user_id = Request["w"];
-        //    string department_id = Request["d"];
-
-        //    int idx_it = Convert.ToInt32(Request["idx"]);
-        //    int auth = Convert.ToInt16(Request.Cookies["check_auth"].Value);
-        //    string web_department_id = Request.Cookies["web_department_id"].Value ?? "";
-
-        //    string ok_auth = "N";
-
-        //    if (user_id == user)
-        //    {
-        //        ok_auth = "Y";
-        //    }
-
-        //    if (web_department_id == department_id)
-        //    {
-        //        ok_auth = "Y";
-        //    }
-
-        //    if (ok_auth == "Y")
-        //    {
-        //        cal_memo _cal_memo = (from a in db.cal_memo where a.idx == idx_it select a).FirstOrDefault();
+            if (user_id == user)
+            {
+                ok_auth = "Y";
+            }
+            if (auth >= 8)
+            {
+                ok_auth = "Y";
+            }
+           
 
 
-        //        var _insert = new work_list
-        //                          {
-        //                              company_id = _cal_memo.company_id,
-        //                              department_id = _cal_memo.department_id,
-        //                              user_id = _cal_memo.user_id,
-        //                              gubun_code = 0,
-        //                              title = _cal_memo.title,
-        //                              gubun_type = _cal_memo.gubun_type,
-        //                              use_yn = "Y",
-        //                              who = User.Identity.Name,
-        //                              write_date = DateTime.Now,
-        //                              work_date = start,
-        //                              edit_date = DateTime.Now,
-        //                              end_date = end,
-        //                              content = _cal_memo.content,
-        //                              state = 1,
-        //                              index = 1,
-        //                              project_idx = _cal_memo.project_idx,
-        //                          };
-        //        db.work_list.Add(_insert);
-        //        db.SaveChanges(); // 실제로 저장 
-        //        state = "Y";
-        //    }
+            if (ok_auth == "Y")
+            {
+                #region 칼린더 드래그 수정
 
+                calendar _update =
+                    (from a in db.calendar where a.idx == idx_it select a).FirstOrDefault();
 
-        //    return state;
-        //}
+                _update.start_date = _start;
+                _update.end_date = _end;
+                _update.edit_date = DateTime.Now;
+
+                db.SaveChanges(); // 실제로 저장  
+                state = "Y";
+
+                #endregion
+            }
+            return state;
+        }
+
 
 
         //[Authorize]
@@ -613,67 +554,78 @@ namespace SmartFactory.Controllers
         //}
 
         ////캘린더 아이템을 가져온다.
-        //public string GetCalendarItem(string _type, string open)
-        //{
-        //    string json = "";
-        //    string web_company_id = Request.Cookies["web_company_id"].Value ?? "";
-        //    string web_department_id = Request.Cookies["web_department_id"].Value ?? "";
-
-        //    int auth = Convert.ToInt16(Request.Cookies["check_auth"].Value);
-        //    int project = 0;
-        //    int category = 0;
+        public string GetCalendarItem(string _type, string open, string d)
+        {
+            string json = "";
+        
 
 
-        //    if (!string.IsNullOrEmpty(open))
-        //    {
-        //        project = Convert.ToInt32(open);
-        //    }
-        //    if (!string.IsNullOrEmpty(_type))
-        //    {
-        //        category = Convert.ToInt32(_type);
-        //    }
+            #region 기본 사용자 정보
+            string user_id = User.Identity.Name;
+            int department_idx = Convert.ToInt32(UserData.user_get(user_id, "department_idx"));
+            int company_idx = Convert.ToInt32(UserData.user_get(user_id, "company_idx"));
+            int auth = Convert.ToInt32(UserData.user_get(user_id, "auth"));
+            #endregion
 
 
-        //    string pageUrl = Request.RawUrl;
-        //    var jr = new JKRegex();
-        //    string site_name = jr.GetHostUrl(pageUrl);
 
 
-        //    var calData = (from a in db.work_list
-        //                   where a.company_id == web_company_id && (a.open_it == "Y" || a.department_id == web_department_id)
-        //                   select new
-        //                              {
-        //                                  id = a.idx,
-        //                                  a.title,
-        //                                  start = a.work_date,
-        //                                  end = a.end_date,
-        //                                  who = a.department.department_name,
-        //                                  project = a.project_idx,
-        //                                  a.state,
-        //                                  a.code_index.gubun,
-        //                                  a.user_id,
-        //                                  a.department_id
-        //                              }).ToList();
-
-        //    if (!string.IsNullOrEmpty(open))
-        //    {
-        //        calData = calData.Where(s => s.project == project).ToList();
-        //    }
-
-        //    if (!string.IsNullOrEmpty(_type))
-        //    {
-        //        calData = calData.Where(s => s.state == category).ToList();
-        //    }
+            int project = 0;
+            int category = 0;
 
 
-        //    json = JsonConvert.SerializeObject(calData);
+            if (!string.IsNullOrEmpty(open))
+            {
+                project = Convert.ToInt32(open);
+            }
+            if (!string.IsNullOrEmpty(_type))
+            {
+                category = Convert.ToInt32(_type);
+            }
 
 
-        //    //json
-        //    string stop = string.Empty;
+           
 
-        //    return json;
-        //}
+            var calData = (from a in db.calendar
+                           where a.company_idx == company_idx && (a.open_it == "Y" || a.department_idx == department_idx)
+                           select new
+                           {
+                               id = a.idx,
+                               a.title,
+                               start = a.start_date,
+                               end = a.end_date,
+                               who = user_id,
+                               project = a.project_idx,
+                               a.code_work_idx,
+                               a.code_index_idx,
+                               a.user_id,
+                               a.department_idxNavigation.department_name,
+                               a.department_idx 
+                           }).ToList();
+
+            if (!string.IsNullOrEmpty(open))
+            {
+                calData = calData.Where(s => s.project == project).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(_type))
+            {
+                calData = calData.Where(s => s.code_work_idx == category).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(d))
+            {
+                department_idx = Convert.ToInt32(d);
+                calData = calData.Where(s => s.department_idx == department_idx).ToList();
+            }
+            json = JsonConvert.SerializeObject(calData);
+
+
+            //json
+            string stop = string.Empty;
+
+            return json;
+        }
 
 
 
